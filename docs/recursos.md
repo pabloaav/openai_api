73d8b24ed2ae57720ec92c53c88316a05a7e17bf
pablo <pablovicentin84@gmail.com>
2025-09-17 00:32:13 -0300

    python openai API integration

---
+++ docs/recursos.md
@@ -0,0 +1,7 @@
+# OpenAI API Intregation
+
+Utilizar modelos de OpenIA Platform para generar soluciones de software 
+
+## Recursos
+- [Comparar modelos](https://ai-sdk.dev/playground)
+- [Ranking de modelos por votacion](https://lmarena.ai/leaderboard)
---
+++ src/main.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+import os
+from dotenv import load_dotenv
+
+from openai import OpenAI
+
+# Load environment variables
+load_dotenv()
+
+# Initialize OpenAI client
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
+def chat_completion(messages: list[dict]):
+    try:
+        response = client.chat.completions.create(
+            model="gpt-3.5-turbo",  # Modelo más económico
+            messages=messages
+        )
+        return response.choices[0].message.content
+    except Exception as e:
+        return f"Error: {e}"
+messages = list([
+    {
+        "role": "system",
+        "content": "Eres un asistente experto de la app SysNetVision presentate como tal."
+    },
+    {
+        "role": "user",
+        "content": "Hola como estas?."
+    }
+])
+if __name__ == "__main__":
+    print(chat_completion(messages))
